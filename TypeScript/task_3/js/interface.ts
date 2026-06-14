e1d3a4b5d67dc78d3efe96cdf7305fa871608367
Fai AlSharekh <fai.alsharekh@gmail.com>
2026-06-14 14:30:07 +0300

    create

---
+++ TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define RowID type alias
+export type RowID = number;
+
+// Define RowElement interface structure layout
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number; // Optional attribute
+}
