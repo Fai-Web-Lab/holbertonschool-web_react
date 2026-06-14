e33488a5a84c42e92fbd676441ece3dde83bb6ec
Fai AlSharekh <fai.alsharekh@gmail.com>
2026-06-14 14:45:16 +0300

    create

---
+++ TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+  // Declaration merging: adding an optional field to the Teacher interface
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher || !this.teacher.experienceTeachingReact) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
